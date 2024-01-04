f2f427fe7a96f2cd7e779c6b4b6ba0c2b24dc35f
Leonardo Luz <leonardoluz10@hotmail.com>
2024-01-04 19:13:07 -0300

    feat: :white_check_mark: Funções FiltraPesquisa e filtrarPorCategoria concluído.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Executar o json-server
+## Obs: é necessário o uso do powerShell como administrador.
+
+`Para liberar o script json-server comando no PowerShell como admin, para rodar a api fake`  
+
+* Coloque  "Get-ExecutionPolicy"  
+* Irá aparecer restrito -> "Restricted", 
+* Execute o comando "Set-ExecutionPolicy RemotesSigned" -> coloque (S) para autorizar.
+* Vai liberar a leitura do script json-server `
+* Após o uso coloque o comando "Set-ExecutionPolicy Restricted" para bloquear novamente após o uso da api.
--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const containerVideos = document.querySelector('.videos__container');
+const barraDePesquisa = document.querySelector('.pesquisar__input');
+const botaoCategoria = document.querySelectorAll('.superior__item');
+
 
 async function buscarEMostar() {
     try {
         const api = await fetch("http://localhost:3000/videos");
         const videos = await api.json();
         videos.forEach((video) => {
-            if(video.categoria == '') throw new Error('Video não tem categoria');
+            if (video.categoria == '') throw new Error('Video não tem categoria');
             containerVideos.innerHTML += `
                 <li class="videos__item">
                     <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -13,6 +16,7 @@ async function buscarEMostar() {
                       <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
                       <h3 class="titulo-video">${video.titulo}</h3>
                       <p class="titulo-canal">${video.descricao}</p>
+                      <p class="categoria" hidden>${video.categoria}</p>
                     </div>
                 </li>
                 `;
@@ -24,3 +28,30 @@ async function buscarEMostar() {
 }
 
 buscarEMostar();
+
+barraDePesquisa.addEventListener('input', filtraPesquisa);
+
+function filtraPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+
+    videos.forEach(video => {
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    })
+}
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute('name');
+    botao.addEventListener('click', () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll('.videos__item');
+    videos.forEach((video) => {
+        const categoria = video.querySelector('.categoria').textContent.toLowerCase();
+        const valorFiltro = filtro.toLowerCase();
+        video.style.display = !categoria.includes(valorFiltro) && valorFiltro != 'tudo' ? 'none' : 'block';
+    });
+};
